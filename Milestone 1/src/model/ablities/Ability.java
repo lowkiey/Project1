d0e9789b33be72c27b26011f9fc271e41f45c519
lowkiey <amr.mohamed.elhilaly2004@gmail.com>
2022-03-19 16:19:25 +0200

    added new file

---
+++ Milestone 1/src/model/ablities/Ability.java
@@ -0,0 +1,43 @@
+package model.ablities;
+public class Ability {
+private String name;
+public String getName() {
+	return name; } 
+private int manaCost;
+public int getManaCost() {
+	return manaCost;
+}
+private int baseCooldown;
+private int currentCooldown;
+private int castRange;
+private int requiredActionPoints;
+AreaOfEffect castArea;
+public int getCurrentCooldown() {
+	return currentCooldown;
+}
+public void setCurrentCooldown(int currentCooldown) {
+	this.currentCooldown = currentCooldown;
+}
+public int getBaseCooldown() {
+	return baseCooldown;
+}
+public int getCastRange() {
+	return castRange;
+}
+public int getRequiredActionPoints() {
+	return requiredActionPoints;
+}
+public AreaOfEffect getCastArea() {
+	return castArea;
+}
+public Ability(String name,int cost, int baseCoolDown, int castRange, AreaOfEffect area ,
+int required) {
+	this.name=name;
+	this.manaCost=cost;
+	this.baseCooldown=baseCoolDown;
+	this.castRange=castRange;
+	this.castArea=area;
+	this.requiredActionPoints=required;
+}
+}
+
---
+++ Milestone 1/src/model/effects/Stun.java
@@ -0,0 +1,8 @@
+package model.effects;
+public class Stun extends Effect {
+public Stun(String name, int duration, EffectType type) {
+		super(name, duration, type);
+	}
+
+EffectType DEBUFF;
+}
