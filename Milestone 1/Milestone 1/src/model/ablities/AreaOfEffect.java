61834b88e56c6ceaa14b0ecd0806848cb6f45907
lowkiey <101935336+lowkiey@users.noreply.github.com>
2022-03-22 22:32:49 +0200

    Uploaded: Updated code up to point 10
    point 10 not done

---
+++ Milestone 1/Milestone 1/src/model/ablities/AreaOfEffect.java
@@ -0,0 +1,4 @@
+package model.ablities;
+public enum AreaOfEffect {
+SELFTARGET, SINGLETARGET, TEAMTARGET, DIRECTIONAL, SURROUND
+}
---
+++ Milestone 1/Milestone 1/src/model/effects/EffectType.java
@@ -0,0 +1,4 @@
+package model.effects;
+public enum EffectType {
+BUFF, DEBUFF
+}
